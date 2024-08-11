7d23272fe5e7e52bd99d6dc8f35623de7cb5e7ef
alok2011 <alokranjantiwaricse@gmail.com>
2024-08-11 18:17:25 +0530

    update

--- src/main/java/com/spring/spring_core/model/Address.java
+++ src/main/java/com/spring/spring_core/model/Address.java
@@ -1,80 +1,80 @@
-package com.spring.spring_core.model;
-
-import org.springframework.stereotype.Component;
-
-@Component
-public class Address 
-{
-	private String city = "Indore";
-	private String state = "MP";
-	private String country = "India";
-	private int pinCode = 33333;
-	
-	public Address(String city, String state, String country, int pinCode) {
-		super();
-		System.out.println("Address.Address(4)");
-		this.city = city;
-		this.state = state;
-		this.country = country;
-		this.pinCode = pinCode;
-	}
-	
-	public Address()
-	{
-		System.out.println("Address.Address()");
-	}
-	
-	
-	
-	public String getCity() 
-	{
-		return city;
-	}
-	
-	public void setCity(String city) 
-	{
-		this.city = city;
-	}
-	
-	public String getState() 
-	{
-		return state;
-	}
-	
-	
-
-	public void setState(String state) 
-	{
-		this.state = state;
-	}
-	
-	public String getCountry() 
-	{
-		return country;
-	}
-	
-	public void setCountry(String country) 
-	{
-		this.country = country;
-	}
-	
-	public int getPinCode() 
-	{
-		return pinCode;
-	}
-	
-	public void setPinCode(int pinCode) 
-	{
-		this.pinCode = pinCode;
-	}
-
-	@Override
-	public String toString() {
-		return "Address [city=" + city + ", state=" + state + ", country=" + country + ", pinCode=" + pinCode + "]";
-	}
-	
-	
-	
-	
-	
-}
+//package com.spring.spring_core.model;
+//
+//import org.springframework.stereotype.Component;
+//
+//@Component
+//public class Address 
+//{
+//	private String city = "Indore";
+//	private String state = "MP";
+//	private String country = "India";
+//	private int pinCode = 33333;
+//	
+//	public Address(String city, String state, String country, int pinCode) {
+//		super();
+//		System.out.println("Address.Address(4)");
+//		this.city = city;
+//		this.state = state;
+//		this.country = country;
+//		this.pinCode = pinCode;
+//	}
+//	
+//	public Address()
+//	{
+//		System.out.println("Address.Address()");
+//	}
+//	
+//	
+//	
+//	public String getCity() 
+//	{
+//		return city;
+//	}
+//	
+//	public void setCity(String city) 
+//	{
+//		this.city = city;
+//	}
+//	
+//	public String getState() 
+//	{
+//		return state;
+//	}
+//	
+//	
+//
+//	public void setState(String state) 
+//	{
+//		this.state = state;
+//	}
+//	
+//	public String getCountry() 
+//	{
+//		return country;
+//	}
+//	
+//	public void setCountry(String country) 
+//	{
+//		this.country = country;
+//	}
+//	
+//	public int getPinCode() 
+//	{
+//		return pinCode;
+//	}
+//	
+//	public void setPinCode(int pinCode) 
+//	{
+//		this.pinCode = pinCode;
+//	}
+//
+//	@Override
+//	public String toString() {
+//		return "Address [city=" + city + ", state=" + state + ", country=" + country + ", pinCode=" + pinCode + "]";
+//	}
+//	
+//	
+//	
+//	
+//	
+//}
